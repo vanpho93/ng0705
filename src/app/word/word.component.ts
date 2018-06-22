59a66a758f26b79fba2cd79f8ec549fe75685334
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-22 20:34:33 +0700

    binding event

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-word',
     template: `
-        <p>word component</p>
+        <h4>Word component</h4>
+        <p>Value = {{ value }}</p>
+        <button class="btn btn-success" (click)="increase();">
+            increase
+        </button>
+        <button class="btn btn-danger" (click)="decrease();">
+            decrease
+        </button>
+        <button class="btn btn-warning" (click)="reset();">
+            reset
+        </button>
     `
 })
 
-export class WordComponent {}
+export class WordComponent {
+    value = 1;
+
+    increase() { this.value++; }
+    decrease() { this.value--; }
+    reset() { this.value = 1; }
+}
