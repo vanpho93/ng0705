e01f7ead2768abd3a3dc7cb777e2814b5dcf272a
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-22 21:14:59 +0700

    two ways binding

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   template: `
-      <h4>List Component</h4>
+    <h4>List Component</h4>
+    <p>{{ txtEmail }}</p>
+    <input
+        class="form-control"
+        placeholder="Enter your email"
+        [(ngModel)]="txtEmail"
+    />
   `
 })
 
-export class ListComponent {}
+export class ListComponent {
+    txtEmail = '';
+}
