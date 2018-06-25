148a0fdd081da97508103e0a81aadc9c84bda838
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-25 20:03:03 +0700

    wordInfo

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -4,7 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-words',
   template: `
     <h4>Words Component</h4>
+    <div class="word">
+      <div class="word-container">
+        <h3 class="text-success">{{ wordInfo.en }}</h3>
+        <h3 class="text-danger">{{ wordInfo.isMemorized ? '******' : wordInfo.vn }}</h3>
+      </div>
+      <div class="btn-container">
+        <button class="btn btn-success" *ngIf="wordInfo.isMemorized">Forgot</button>
+        <button class="btn btn-danger" *ngIf="!wordInfo.isMemorized">Memorized</button>
+        <button class="btn btn-warning">
+          Remove
+        </button>
+      </div>
+    </div>
   `
 })
 
-export class WordsComponent {}
+export class WordsComponent {
+  wordInfo = { en: 'One', vn: 'Mot', isMemorized: true };
+}
