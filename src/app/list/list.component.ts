945f1f4d297214db18fbd2cb35dfde9e727f5739
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-25 19:49:16 +0700

    html

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -4,15 +4,34 @@ import { Component } from '@angular/core';
   selector: 'app-list',
   template: `
     <h4>List Component</h4>
-    <p>{{ txtEmail }}</p>
+    <button class="btn btn-success" *ngIf="!shouldShowInput">
+      Add email
+    </button>
     <input
-        class="form-control"
-        placeholder="Enter your email"
-        [(ngModel)]="txtEmail"
+      *ngIf="shouldShowInput"
+      class="form-control"
+      placeholder="Enter your email"
+      [(ngModel)]="txtEmail"
+      (keyUp.enter)="addEmail();"
     />
+    <div>
+      <p *ngFor="let x of emails">{{x}}</p>
+    </div>
   `
 })
 
 export class ListComponent {
-    txtEmail = '';
+  shouldShowInput = false;
+  txtEmail = '';
+  emails = [
+    'teo@gmail.com',
+    'ti@gmail.com',
+    'tun@gmail.com',
+    'tuan@gmail.com',
+  ];
+
+  addEmail() {
+    this.emails.push(this.txtEmail);
+    this.txtEmail = '';
+  }
 }
