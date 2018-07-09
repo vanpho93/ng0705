e6ca035cd4ebd4b94adf00514a754082d501ae90
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-09 20:27:26 +0700

    password

--- src/app/form/sign-up.component.ts
+++ src/app/form/sign-up.component.ts
@@ -9,21 +9,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
           <label for="exampleInputEmail1">Email address</label>
           <input type="email" class="form-control" placeholder="Enter email" formControlName="email">
         </div>
-        <i class="text-danger" style="margin-bottom: 10px; display: block;" *ngIf="formSignUp.get('email').invalid">
-            Email khong hop le
+        <i *ngIf="shouldShowEmailError">
+            *Email khong hop le
         </i>
         <div class="form-group">
           <label for="exampleInputPassword1">Password</label>
           <input type="password" class="form-control" placeholder="Password" formControlName="password">
         </div>
-        <i class="text-danger" style="margin-bottom: 10px; display: block;" *ngIf="formSignUp.get('password').invalid">
-            Password khong hop le
+        <i *ngIf="formSignUp.get('password').invalid && formSignUp.get('password').touched">
+            *Password khong hop le
         </i>
         <button type="submit" class="btn btn-primary" [disabled]="formSignUp.invalid">
             Sign Up
         </button>
     </form>
-    `
+    `,
+    styles: [`
+        i { color: red; display: block; margin-bottom: 10px; }
+    `]
 })
 
 export class SignUpComponent {
@@ -34,5 +37,10 @@ export class SignUpComponent {
 
     signUp() {
         alert(JSON.stringify(this.formSignUp.value));
+    }
+
+    get shouldShowEmailError(): boolean {
+        const emailControl = this.formSignUp.get('email');
+        return emailControl.invalid && emailControl.touched;
     }
 }
