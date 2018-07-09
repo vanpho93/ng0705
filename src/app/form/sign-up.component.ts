462a1aa3397104939334e639e707109fe7f5d1e2
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-09 20:10:16 +0700

    form sign up invalid preventing

--- src/app/form/sign-up.component.ts
+++ src/app/form/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-sign-up',
@@ -13,16 +13,18 @@ import { FormGroup, FormControl } from '@angular/forms';
           <label for="exampleInputPassword1">Password</label>
           <input type="password" class="form-control" placeholder="Password" formControlName="password">
         </div>
-        <button type="submit" class="btn btn-primary">Sign Up</button>
+        <button type="submit" class="btn btn-primary" [disabled]="formSignUp.invalid">Sign Up</button>
     </form>
     <pre>{{ formSignUp.value | json }}</pre>
+    <pre>{{ formSignUp.get('email').errors | json }}</pre>
+    <pre>{{ formSignUp.get('password').errors | json }}</pre>
     `
 })
 
 export class SignUpComponent {
     formSignUp = new FormGroup({
-        email: new FormControl(),
-        password: new FormControl()
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(4)])
     });
 
     signUp() {
